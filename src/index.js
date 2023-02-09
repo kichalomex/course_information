43c6f52a5e2be9413d4775735d8d75396d910dd1
kichalomex <kichalo23con@hotmail.com>
2023-02-08 18:01:35 -0600

    paso 2 hecho
    se realizo el paso 2 del pdf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () =>{
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14 
+  return(
+    <div>
+      <Header course={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+const Header = (props) =>
+(<div><h1>{props.course}</h1></div>)
+
+const Content = (props) =>
+(<div>
+  <Parts part={props.part1} exercises={props.exercises1}/>
+  <Parts part={props.part2} exercises={props.exercises2}/>
+  <Parts part={props.part3} exercises={props.exercises3}/>
+</div>)
+const Parts = (props) =>
+(<div><p>{props.part} {props.exercises}</p></div>)
+
+const Total = (props) => 
+(<div><p>Number of exercises {props.exercises1 + props.exercises2+ props.exercises3}</p></div>)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +39,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
