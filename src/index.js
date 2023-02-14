508e96e9671e5a676ab6d60624e4eac013236b3c
kichalomex <kichalo23con@hotmail.com>
2023-02-14 16:18:34 -0600

    paso 4
    se realizo el paso 4

--- src/index.js
+++ src/index.js
@@ -3,51 +3,53 @@ import ReactDOM from 'react-dom/client';
 
 const App = () =>{
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+  {
     name:'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name:'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   } 
-
+  ]
   return(
     <div>
       <Header course={course}/>
-      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} 
-      exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total exercises={parts.map(x => x['exercises'])} />
     </div>
   )
 }
 
 const Header = (props) =>{
   console.log(props)
-  return <div><h1>{props.course}</h1></div>
+  return (<div><h1>{props.course}</h1></div>)
 }
 
 const Content = (props) =>{
+  const parts = props.parts
   console.log(props)
-return <div>
-  <Parts part={props.part1} exercises={props.exercises1}/>
-  <Parts part={props.part2} exercises={props.exercises2}/>
-  <Parts part={props.part3} exercises={props.exercises3}/>
-</div>
+return (<div>
+  { parts.map(element =>(
+    <Part part={element.name} exercises={element.exercises}/>)
+  )}
+</div>)
 }
 
-const Parts = (props) =>{
+const Part = (props) =>{
   console.log(props)
-return <div><p>{props.part} {props.exercises}</p></div>
+return (<div><p>{props.part} {props.exercises}</p></div>)
 }
 
 const Total = (props) => {
+  const exercises = props.exercises
   console.log(props)
-return <div><p>Number of exercises {props.exercises1 + props.exercises2+ props.exercises3}</p></div>
+return (<div><p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p></div>)
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
